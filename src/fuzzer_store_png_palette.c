9729aee3ad4cef70921891124022b60114302164
Soogeun Park <bwmelon97@naver.com>
2022-06-09 12:11:49 +0000

    feat: implement fuzzer_store_png_palette

---
+++ src/fuzzer_store_png_palette.c
@@ -0,0 +1,18 @@
+#include "pngparser.h"
+#include <stdio.h>
+
+// LibFuzzer stub
+//
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    // plte_chunk is 4 byte length
+    if (Size <= 0 || Size % 4 != 0) {
+        return 0;
+    }
+    
+    struct image *test_img;
+    store_png("out.png", &test_img, Data, (Size / 4));
+    free(test_img);
+
+    // Always return 0
+    return 0;
+}
